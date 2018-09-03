e2f86a3dd21ca3753aac6cd59afc6c92c8b4a77e
Zhang Jun <zhangjun@4paradigm.com>
2018-09-03 22:16:03 +0800

    update

--- manifests/environment.sh
+++ manifests/environment.sh
@@ -13,7 +13,7 @@ export ETCD_ENDPOINTS="https://172.27.136.1:2379,https://172.27.136.2:2379,https
 export ETCD_NODES="m7-demo-136001=https://172.27.136.1:2380,m7-demo-136002=https://172.27.136.2:2380,m7-demo-136003=https://172.27.136.3:2380"
 
 # kube-apiserver 的 VIP（HA 组件 keepalived 发布的 IP）
-export MASTER_VIP=172.27.128.251
+export MASTER_VIP=172.27.136.254 # 254 - 集群第一个 IP 的最后一段值
 
 # kube-apiserver VIP 地址（HA 组件 haproxy 监听 8443 端口）
 export KUBE_APISERVER="https://${MASTER_VIP}:8443"
@@ -22,10 +22,10 @@ export KUBE_APISERVER="https://${MASTER_VIP}:8443"
 export VIP_IF="eth0"
 
 # keepalived 的 virtual_router_id 值，位于 [0, 255] 之间
-export VIRTUAL_ROUTER_ID=80 # 从 80 开始递增
+export VIRTUAL_ROUTER_ID=80 # 80 + 集群第一个 IP 的最后一段值
 
 # keepalived 的 router_id 值
-export ROUTER_ID=136001 # IP 后两段
+export ROUTER_ID=136001 # 群第一个 IP 的最后两段值
 
 # etcd 数据目录
 export ETCD_DIR="/mnt/disk01/etcd"

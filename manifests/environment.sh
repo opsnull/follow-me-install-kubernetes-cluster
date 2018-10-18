083b3c774f11056f44b28eb7c3346f1e5ef19519
Zhang Jun <zhangjun@4paradigm.com>
2018-10-18 11:29:29 +0800

    environment.sh: export variables. fixed #336

--- manifests/environment.sh
+++ manifests/environment.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
 # 生成 EncryptionConfig 所需的加密 key
-ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
+export ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
 
 # 最好使用 当前未用的网段 来定义服务网段和 Pod 网段
 
 # 服务网段，部署前路由不可达，部署后集群内路由可达(kube-proxy 和 ipvs 保证)
-SERVICE_CIDR="10.254.0.0/16"
+export SERVICE_CIDR="10.254.0.0/16"
 
 # Pod 网段，建议 /16 段地址，部署前路由不可达，部署后集群内路由可达(flanneld 保证)
-CLUSTER_CIDR="172.30.0.0/16"
+export CLUSTER_CIDR="172.30.0.0/16"
 
 # 服务端口范围 (NodePort Range)
 export NODE_PORT_RANGE="8400-9000"

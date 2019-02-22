f949d3c0e153d7e2592ec5802051ee806d4616d6
Zhang Jun <zhangjun@4paradigm.com>
2019-02-22 17:30:39 +0800

    update to k8s 1.12

--- manifests/environment.sh
+++ manifests/environment.sh
@@ -3,9 +3,44 @@
 # 生成 EncryptionConfig 所需的加密 key
 ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
 
+# 集群各机器 IP 数组
+export NODE_IPS=(172.27.128.179 172.27.128.101 172.27.128.102)
+
+# 集群各 IP 对应的 主机名数组
+export NODE_NAMES=(m7-inf-prod01 m7-common-dfs03 m7-common-dfs02 )
+
+# etcd 集群服务地址列表
+export ETCD_ENDPOINTS="https://172.27.128.179:2379,https://172.27.128.101:2379,https://172.27.128.102:2379"
+
+# etcd 集群间通信的 IP 和端口
+export ETCD_NODES="m7-inf-prod01=https://172.27.128.179:2380,m7-common-dfs03=https://172.27.128.101:2380,m7-common-dfs02=https://172.27.128.102:2380"
+
+# kube-apiserver 的反向代理(kube-nginx)地址端口
+export KUBE_APISERVER="https://127.0.0.1:8443"
+
+# 节点间互联网络接口名称
+export IFACE="eth0"
+
+# etcd 数据目录
+export ETCD_DATA_DIR="/data/k8s/etcd/data"
+
+# etcd WAL 目录，建议是 SSD 磁盘分区，或者和 ETCD_DATA_DIR 不同的磁盘分区
+export ETCD_WAL_DIR="/data/k8s/etcd/wal"
+
+# k8s 各组件数据目录
+export K8S_DIR="/data/k8s/k8s"
+
+# docker 数据目录
+export DOCKER_DIR="/data/k8s/docker"
+
+## 以下参数一般不需要修改
+
+# TLS Bootstrapping 使用的 Token，可以使用命令 head -c 16 /dev/urandom | od -An -t x | tr -d ' ' 生成
+BOOTSTRAP_TOKEN="41f7e4ba8b7be874fcff18bf5cf41a7c"
+
 # 最好使用 当前未用的网段 来定义服务网段和 Pod 网段
 
-# 服务网段，部署前路由不可达，部署后集群内路由可达(kube-proxy 和 ipvs 保证)
+# 服务网段，部署前路由不可达，部署后集群内路由可达(kube-proxy 保证)
 SERVICE_CIDR="10.254.0.0/16"
 
 # Pod 网段，建议 /16 段地址，部署前路由不可达，部署后集群内路由可达(flanneld 保证)
@@ -13,27 +48,6 @@ CLUSTER_CIDR="172.30.0.0/16"
 
 # 服务端口范围 (NodePort Range)
 export NODE_PORT_RANGE="30000-32767"
-
-# 集群各机器 IP 数组
-export NODE_IPS=(172.27.128.150 172.27.128.149 172.27.128.148)
-
-# 集群各 IP 对应的 主机名数组
-export NODE_NAMES=(m7-autocv-gpu01 m7-autocv-gpu02 m7-autocv-gpu03)
-
-# kube-apiserver 的 VIP（HA 组件 keepalived 发布的 IP）
-export MASTER_VIP=172.27.129.253
-
-# kube-apiserver VIP 地址（HA 组件 haproxy 监听 8443 端口）
-export KUBE_APISERVER="https://${MASTER_VIP}:8443"
-
-# HA 节点，配置 VIP 的网络接口名称
-export VIP_IF="eth0"
-
-# etcd 集群服务地址列表
-export ETCD_ENDPOINTS="https://172.27.128.150:2379,https://172.27.128.149:2379,https://172.27.128.148:2379"
-
-# etcd 集群间通信的 IP 和端口
-export ETCD_NODES="m7-autocv-gpu01=https://172.27.128.150:2380,m7-autocv-gpu02=https://172.27.128.149:2380,m7-autocv-gpu03=https://172.27.128.148:2380"
 
 # flanneld 网络配置前缀
 export FLANNEL_ETCD_PREFIX="/kubernetes/network"
@@ -44,7 +58,7 @@ export CLUSTER_KUBERNETES_SVC_IP="10.254.0.1"
 # 集群 DNS 服务 IP (从 SERVICE_CIDR 中预分配)
 export CLUSTER_DNS_SVC_IP="10.254.0.2"
 
-# 集群 DNS 域名，注意域名不以 . 结尾
+# 集群 DNS 域名（末尾不带点号）
 export CLUSTER_DNS_DOMAIN="cluster.local"
 
 # 将二进制目录 /opt/k8s/bin 加到 PATH 中
